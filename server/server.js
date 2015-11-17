7f337ea9b339645da82fef6bc2bc8d4880e20e86
njethani <nishant.jethani@gmail.com>
2015-11-17 14:54:07 -0800

    chaning port

--- server/server.js
+++ server/server.js
@@ -9,7 +9,9 @@ app.start = function() {
   // start the web server
   return app.listen(function() {
     app.emit('started');
+    app.set('url',"http://54.193.16.83:3000")
     var baseUrl = app.get('url').replace(/\/$/, '');
+    console.log(baseUrl);
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
