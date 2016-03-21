d3eb09e3be2f096d9ffeeb8fc820b051316be384
WillBMease <will@jellyvibes.com>
2016-03-21 10:31:33 -0700

    d

--- api.js
+++ api.js
@@ -26,6 +26,9 @@ for (var i = 0 ; i < videos.length ; i++){
 }
 
 console.log(ytVideos)
+for (var i = 0 ; i < 10 ; i++){
+  console.log(i)
+}
 for (var i = 0 ; i < ytVideos.length ; i++){
   console.log(i)
   console.log(ytVideos[i].src)
