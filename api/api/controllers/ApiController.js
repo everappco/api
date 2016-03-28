89399b323d17f9cfc7cf78bada071169850c9416
WillBMease <will@jellyvibes.com>
2016-03-28 11:18:34 -0700

    d

---
+++ api/api/controllers/ApiController.js
@@ -0,0 +1,21 @@
+/**
+ * ApiController
+ *
+ * @description :: Server-side logic for managing apis
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+	collectAPI: function(req, res) {
+		API.findOne(uniqueID: req.body.uniqueID).exec(function (err, found){
+			if (found){
+				res.send(found.api)
+			}
+			else {
+				res.send(false)
+			}
+		})
+	}
+
+};
